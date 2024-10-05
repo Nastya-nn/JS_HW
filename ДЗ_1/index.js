c7806ddd1c42559d5370846657411cede648d9f8
Nastya <nnnastyaaa533@gmail.com>
2024-10-05 20:54:25 +0300

    Версия ДЗ_1 до правок

---
+++ ДЗ_1/index.js
@@ -0,0 +1,84 @@
+console.log('Hello World!');
+
+
+let age = 25;
+console.log(age);
+console.log(typeof age);
+console.log(typeof(age));
+
+
+let name = 'Nastya';
+console.log(name);
+console.log(typeof name);
+console.log(typeof(name));
+
+
+let isPeople = true;
+console.log(isPeople);
+console.log(typeof isPeople);
+console.log(typeof(isPeople));
+
+
+let children = null;
+console.log(children);
+console.log(typeof children);
+console.log(typeof(children));
+
+
+let price;
+console.log(price);
+console.log(typeof price);
+console.log(typeof(price));
+
+
+let user = {
+    firstName: 'Ivan',
+    lastName: 'Ivanov',
+};
+console.log(user);
+console.log(typeof user);
+console.log(typeof(user));
+
+
+let bigint = 123456789012345n;
+console.log(bigint);
+console.log(typeof bigint);
+console.log(typeof(bigint));
+
+
+let id = Symbol();
+console.log(id);
+console.log(typeof id);
+console.log(typeof(id));
+
+
+const date = {
+    day: 27,
+    month: 'September',
+    year: 2024
+};
+date.day = 26;
+console.log(date);
+
+
+let address = {
+    city: 'Moscow',
+    street: 'Krasnaya',
+    office: 65
+};
+address.street = 'Kensington';
+console.log(address);
+
+
+var dogBob = {
+    nickname: 'Bob',
+    color: 'black',
+    breed: 'bulldog'
+};
+dogBob.color = 'brown';
+console.log(dogBob);
+
+
+const userName = 'Alex';
+userName = 'Sony';
+console.log(userName);
